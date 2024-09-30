a550bb28737fb570b26079e8e6cf1f62ea9147c1
Jamil Rahman <fx78901@gmail.com>
2024-09-30 19:31:08 +0100

    added a find user function which travels to the users exact location

--- src/MapGPS.jsx
+++ src/MapGPS.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet';
+import React, { useState } from 'react'
+import { MapContainer, TileLayer, useMapEvents, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+
+function FindUser()
+ {
+    const [position, setPosition] = useState(null)
+    const map = useMapEvents({
+      click() {
+        map.locate({enableHighAccuracy: true})
+      },
+      locationfound(e) {
+        setPosition(e.latlng)
+        map.flyTo(e.latlng, map.getZoom())
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position = {position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+ }
 
 function MapGPS(){
+
     return(
-  <MapContainer style={{ height: "500px", width: "800%" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <MapContainer style={{ height: "500px", width: "800%" }} center={[54.0104, -2.7877]} zoom={15} scrollWheelZoom={true}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -13,7 +36,8 @@ function MapGPS(){
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
   </Popup>
-  </Marker>
+</Marker>
+<FindUser></FindUser>
 </MapContainer>
     );
 }
