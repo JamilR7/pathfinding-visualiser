de74644fca8b1936cbb01499193c1169891dd9b1
Jamil Rahman <fx78901@gmail.com>
2024-09-30 17:53:59 +0100

    initial commit, add ed map from leaflet

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapGPS from './MapGPS.jsx';
+
+function App() {
+
+  return (
+    <div className = "App">
+      <h1>Map</h1>
+      <MapGPS></MapGPS>
+    </div>
+  );
+}
+
+export default App
