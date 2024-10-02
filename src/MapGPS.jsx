5a0b40a1367c2cb11071c437f174f269dcadceee
Jamil Rahman <fx78901@gmail.com>
2024-10-02 21:59:22 +0100

    added some local food stores

--- src/MapGPS.jsx
+++ src/MapGPS.jsx
@@ -8,7 +8,7 @@ function FindUser({ isLocating, setIsLocating }) {
   const [position, setPosition] = useState(null)
   const [accuracy, setAccuracy] = useState(null)
   const map = useMapEvents({
-    locationfound: (location)=> {
+    locationfound: (location) => {
       if (isLocating) {
         setPosition(location.latlng)
         setAccuracy(location.accuracy); // Store the accuracy in meters
@@ -19,50 +19,55 @@ function FindUser({ isLocating, setIsLocating }) {
   })
 
   if (isLocating) {
-    map.locate({maximumAge: 0, enableHighAccuracy: true });
+    map.locate({ maximumAge: 0, enableHighAccuracy: true });
   }
 
-    return position === null ? null : (
+  return position === null ? null : (
     <Marker position={position}>
       <Popup>
         You are here
         Accuracy: {accuracy ? `${accuracy} meters` : 'Unknown'} {/* Display the accuracy */}
       </Popup>
     </Marker>
-    )
+  )
 
 }
 
-function MapGPS(){
+function MapGPS() {
 
-    const [isLocating, setIsLocating] = useState(false);
+  const [isLocating, setIsLocating] = useState(false);
 
-    const handleButtonClick = () => {
-      setIsLocating(true);
-    };
+  const handleButtonClick = () => {
+    setIsLocating(true);
+  };
 
-    return (
-      <div>
-        <button
-          className='FindUserButton'
-          onClick={handleButtonClick}
-        >
-          Detect location
-        </button>
-  <MapContainer style={{ height: "500px", width: "800%" }} center={[54.0104, -2.7877]} zoom={15} scrollWheelZoom={true}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-  </Popup>
-</Marker>
-<FindUser isLocating={isLocating} setIsLocating={setIsLocating}></FindUser>
-</MapContainer>
-      </div>
-    );
+  return (
+    <div>
+      <button
+        className='FindUserButton'
+        onClick={handleButtonClick}
+      >
+        Detect location
+      </button>
+      <MapContainer style={{ height: "500px", width: "800%" }} center={[54.0104, -2.7877]} zoom={15} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[54.00375, -2.788841]}></Marker>
+        <Marker position={[54.0102751, -2.7854227]}></Marker>
+        <Marker position={[54.010122, -2.7852353]}></Marker>
+        <Marker position={[54.007509, -2.784629]}></Marker>
+        <FindUser isLocating={isLocating} setIsLocating={setIsLocating}></FindUser>
+        <FindUser isLocating={isLocating} setIsLocating={setIsLocating}></FindUser>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default MapGPS
